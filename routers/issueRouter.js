dccf6cd4ef86d792a29f559fd3209b0f835e08c9
Anthony Natoli <anatoli@purdue.edu>
2015-07-05 20:22:47 -0400

    ctb-9 POST route complete, returns created document on success

--- routers/issueRouter.js
+++ routers/issueRouter.js
@@ -1,5 +1,11 @@
 var express = require( 'express' );
 var mongoose = require( 'mongoose' );
+var bodyParser = require( 'body-parser' );
+var multer = require( 'multer' );
+var _ = require ( 'underscore' );
+
+var app = express();
+var jsonParser = bodyParser.json();
 
 var router = express.Router();
 
@@ -7,17 +13,25 @@ var Issue = require( '../schemas/issue' );
 
 router.get( '/', function( req, res ){
 	console.log( req.query );
-	Issue.find( req.query, function (err, docs) {
-    	if (err){
-			return next(err);
-    	}
+	Issue.find( req.query, function( err, docs ){
+		if (err){
+			return res.send(err);
+		}
 
-    	res.send(docs);
-  	});
+		res.send(docs);
+	});
 });
 
-router.post( '/', function( req, res ){
-	res.send( 'You sent a POST request to the server' );
+router.post( '/', jsonParser, function( req, res ){
+	var issue = req.body;
+	var new_doc = new Issue( issue );
+	Issue.create( new_doc, function( err, doc ){
+		if( err ){
+			return res.send( err );
+		}
+
+		res.send( doc );
+	});
 });
 
 module.exports = router;
