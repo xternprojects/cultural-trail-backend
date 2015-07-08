decd7907b7e79e7f1b1fcfff3b90dece725840e7
Anthony Natoli <anatoli@purdue.edu>
2015-07-07 23:09:38 -0400

    CTB-15 reportedDate now generated at time of POST request by server

--- routers/issueRouter.js
+++ routers/issueRouter.js
@@ -2,6 +2,7 @@ var express = require( 'express' );
 var mongoose = require( 'mongoose' );
 var bodyParser = require( 'body-parser' );
 var multer = require( 'multer' );
+var moment = require( 'moment' );
 
 var app = express();
 var jsonParser = bodyParser.json();
@@ -23,6 +24,8 @@ router.get( '/', function( req, res ){
 
 router.post( '/', jsonParser, function( req, res ){
 	var issue = req.body;
+	issue.reportedDate = moment().format( 'MM-DD-YYYY hh:mm a');
+	issue.resolvedDate = null;
 	var new_doc = new Issue( issue );
 	Issue.create( new_doc, function( err, doc ){
 		if( err ){
