02cdc39a9ec95c55cb95d66f42e6c574fbd0d3d1
Anthony Natoli <anatoli@purdue.edu>
2015-07-12 19:39:07 -0400

    Start using environment variables to increase security

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 var express = require( 'express' );
 var http = require( 'http' );
 var mongoose = require( 'mongoose' );
+var dotenv = require( 'dotenv' );
 
 var app = express();
 
-var PORT = 3000;
+dotenv.load(); //load environment variables for local development, heroku variables will be the same
 
-global.db = mongoose.createConnection( 'mongodb://xtern:2015@ds035280.mongolab.com:35280/heroku_btspb3kw' );
+global.db = mongoose.createConnection( process.env.MONGOLAB_URI );
 
 var server = http.createServer( app );
 
 var issueRouter = require( './routers/issueRouter' );
 
-app.get( '/', function( req, res ) {
-	res.send( 'Welcome!' );
-});
-
 app.use( '/issues', issueRouter );
 
 //process.env.PORT defined by heroku
-server.listen( process.env.PORT || PORT, function () {
+server.listen( process.env.PORT, function () {
 	console.log( 'listening' );
 });
