f4898d4abe72d3d95a61fb1ee6bbef214ef246e8
Anthony Natoli <anatoli@purdue.edu>
2015-07-12 22:29:10 -0400

    CTB-23 Sends auth information properly now

--- routers/pictureRouter.js
+++ routers/pictureRouter.js
@@ -12,7 +12,14 @@ router.get( '/', function( req, res ){
 	params += req.query.public_id ? ( 'public_id=' +  req.query.public_id + '&' ) : null;
 	params += 'timestamp=' + timestamp + process.env.CLOUDINARY_SECRET;
 	shasum.update( params );
-	res.send( shasum.digest( 'hex' ) );
+	var signature = shasum.digest( 'hex' );
+	var auth = {
+		'signature': signature
+		,'public_id': process.env.CLOUDINARY_CLOUD_NAME
+		, 'timestamp': timestamp
+		, 'api_key': process.env.CLOUDINARY_KEY
+	}
+	res.send( auth );
 
 });
 
