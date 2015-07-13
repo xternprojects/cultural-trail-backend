5a8d82cb5c095ee64ebf35a058fe115bacafc62d
Anthony Natoli <anatoli@purdue.edu>
2015-07-12 23:14:25 -0400

    Fixed another stupid mistake on my part

--- routers/pictureRouter.js
+++ routers/pictureRouter.js
@@ -7,15 +7,16 @@ router.get( '/', function( req, res ){
 	
 	var shasum = crypto.createHash('sha1');
 	var timestamp = Math.round((new Date()).getTime() / 1000); //UNIX timestamp
+	var public_id = req.query.public_id ? req.query.public_id : null;
 	var params = '';
 	
-	params += req.query.public_id ? ( 'public_id=' +  req.query.public_id + '&' ) : null;
+	params += public_id;
 	params += 'timestamp=' + timestamp + process.env.CLOUDINARY_SECRET;
 	shasum.update( params );
 	var signature = shasum.digest( 'hex' );
 	var auth = {
 		'signature': signature
-		,'public_id': process.env.CLOUDINARY_CLOUD_NAME
+		,'public_id': public_id
 		, 'timestamp': timestamp
 		, 'api_key': process.env.CLOUDINARY_KEY
 	}
