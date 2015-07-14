7266ee63f20aee3c311ee4aa27aa735f2da1b3b7
Kyle <pottsk@purdue.edu>
2015-07-14 11:00:51 -0400

    updating to see if this works with android

--- routers/pictureRouter.js
+++ routers/pictureRouter.js
@@ -4,14 +4,14 @@ var crypto = require( 'crypto' );
 var router = express.Router();
 
 router.get( '/auth', function( req, res ){
-	
+
 	var shasum = crypto.createHash('sha1');
 	var timestamp = Math.round((new Date()).getTime() / 1000); //UNIX timestamp
 	var public_id = req.query.public_id ? req.query.public_id : null;
 	var params = '';
-	
+
 	params += public_id ? 'public_id=' + public_id + '&' : '';
-	params += 'timestamp=' + timestamp + process.env.CLOUDINARY_SECRET;
+	params += 'timestamp=' + timestamp;
 	shasum.update( params );
 	var signature = shasum.digest( 'hex' );
 	var auth = {
